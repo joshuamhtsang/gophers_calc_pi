e88cc19cddeb3e846ef833094ba876540a300014
Joshua Tsang <joshua.tsang@soundmouse.com>
2021-08-30 18:34:11 +0100

    estimate of pi without go routines

--- calc_pi.go
+++ calc_pi.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	"math/rand"
 	"github.com/joshuamhtsang/gophers_calc_pi/geometry"
 )
 
@@ -10,4 +10,38 @@ func main() {
 	fmt.Println("Gophers Calculate Pi!")
 
 	fmt.Println(geometry.WithinUnitCircle(0.5, 0.5))
+
+	rand.Seed(45)
+
+	fmt.Println(estimate_pi(10000))
+}
+
+func estimate_pi(num_iterations int) float64 {
+	var inside_quadrant = make([]bool, num_iterations)
+
+	for i := 0; i < num_iterations; i++ {
+		var x_test float64 = rand.Float64()
+		var y_test float64 = rand.Float64()
+
+		inside_quadrant[i] = geometry.WithinUnitCircle(x_test, y_test)
+	}
+
+	var fraction_inside float64 = compute_fraction_inside(inside_quadrant)
+
+	return 4*fraction_inside
+}
+
+func compute_fraction_inside(inside []bool) float64 {
+	var num_inside int = 0
+	var num_samples int = len(inside)
+
+	for i := 0; i < num_samples; i++ {
+		if inside[i] {
+			num_inside++
+		}
+	}
+
+	var fraction float64 = float64(num_inside)/float64(num_samples)
+
+	return fraction
 }
