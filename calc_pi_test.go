f4708ba11d6e769bdc5c8e135ed3a71bd3e9f462
Joshua Tsang <joshua.tsang@soundmouse.com>
2021-09-04 18:53:05 +0100

    forgot to update test function name

--- calc_pi_test.go
+++ calc_pi_test.go
@@ -15,7 +15,7 @@ func Test_estimate_pi(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := estimate_pi(tt.args.num_iterations); got != tt.want {
+			if got := estimatePi(tt.args.num_iterations); got != tt.want {
 				t.Errorf("estimate_pi() = %v, want %v", got, tt.want)
 			}
 		})
